fdaba7c6ccd84337be38dd068c3ad8b57982c17a
Nisha04Suthar <nishasuthar2002@gmail.com>
2025-01-08 22:06:31 +0530

    previous and next button added

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,34 +4,63 @@ import data from './assets/data'
 
 function Main() {
 
-  let [index, setIndex] = useState(0);
-  let [question, setQuestion] = useState(data[index]);
+  const [index, setIndex] = useState(0);
+  const [question, setQuestion] = useState(data[index]);
+  const [isCorrect, setIsCorrect] = useState(null);
+  const [selectedOption, setSelectedOption] = useState(null);
 
   const NextQuestion = () =>{
     const newIndex = index+1;
     if(newIndex<data.length){
       setIndex(newIndex);
       setQuestion(data[newIndex]);
+      setIsCorrect(null);
+      setSelectedOption(null);
     }
   };
 
+  const PrevQuestion=()=>{
+    const newIndex = index-1;
+    if(newIndex>=0){
+      setIndex(newIndex);
+      setQuestion(data[newIndex]);
+      setIsCorrect(null);
+      setSelectedOption(null);
+    }
+  }
+
+  const handleOption = (option) =>{
+    setSelectedOption(option);  // Track the selected option
+    setIsCorrect(option === question.answer);
+  }
+
   return (
-    <>
-        <div className="quiz-card">
-            <div className="quiz">
-                <div className="question">
-                  <h3>{index+1}. {question.question}</h3>
-                </div>
-                {question.options.map((option,index)=>(
-                  <div key={index} className="option">
-                    {option}
+    <>  
+          <div className="quiz-card">
+              <div className="quiz">
+                  <div className="question">
+                    <h3>{question.id}. {question.question}</h3>
                   </div>
-                ))}
-            </div>
-            <div className="btn">
-              <button type='submit' onClick={NextQuestion}>Next</button>
-            </div>
-        </div>
+                  {question.options.map((option,index)=>(
+                    <div key={index} 
+                        className={`option 
+                          ${selectedOption !== null ? 
+                            option === question.answer ? 'correct' : 
+                            option === selectedOption ? 'wrong' : 
+                            '' 
+                          : ''}
+                        `} 
+                          onClick={()=>handleOption(option)}>
+                      {option}
+                    </div>
+                  ))}
+              </div>
+              <div className="btn">
+                <button onClick={PrevQuestion}>Prev</button>
+                <p>{index+1} of {data.length} questions</p>
+                <button onClick={NextQuestion}>Next</button>
+              </div>
+          </div>
     </>
   )
 }
